59e004b781772b0c96ef9418e4afd6c971d1e9ad
yashugowda-123 <gyashaswini11@gmail.com>
2025-08-03 12:41:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-from flask import Flask, request, render_template
-from PyPDF2 import PdfReader
+import streamlit as st
 
-app = Flask(_name_)
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_pdf():
-    if request.method == 'POST':
-        file = request.files['pdf']
-        reader = PdfReader(file)
-        text = reader.pages[0].extract_text()
-        return f"<pre>{text}</pre>"
-    return '''
-        <form method="post" enctype="multipart/form-data">
-            <input type="file" name="pdf" accept=".pdf">
-            <input type="submit">
-        </form>
-    '''
-
-if _name_ == '_main_':
-    app.run(debug=True)
+st.title("Welcome to My App!")
+st.write("This is the homepage.")
